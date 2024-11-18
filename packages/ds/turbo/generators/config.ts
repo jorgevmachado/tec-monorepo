b527d481020624f609dc73643de0877dacb0c5e7
Jorge Machado <jorge.vmachado@hotmail.com>
2024-11-17 21:59:26 -0300

    implement double prompt in turbo gen ds

--- packages/ds/turbo/generators/config.ts
+++ packages/ds/turbo/generators/config.ts
@@ -1,13 +1,13 @@
 import { PlopTypes } from '@turbo/gen';
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
-  plop.setGenerator('ds-element', {
-    description: 'add a new design system element',
+  plop.setGenerator('ds', {
+    description: 'add a new design system',
     prompts: [
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of the design system element?',
+        message: 'What is the name of the design system item ?',
         validate: (input: string) => {
           if (input.includes('.')) {
             return 'file name cannot include an extension';
@@ -21,82 +21,39 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
           return true;
         },
       },
+      {
+        type: 'list',
+        name: 'type',
+        message: 'What type of file should be created ?',
+        choices: ['elements', 'components']
+      }
     ],
     actions: [
       {
         type: 'add',
-        path: 'src/elements/{{ kebabCase name }}/{{ pascalCase name }}.tsx',
+        path: 'src/{{ type }}/{{ kebabCase name }}/{{ pascalCase name }}.tsx',
         templateFile: 'templates/ds.hbs',
       },
       {
         type: 'add',
-        path: 'src/elements/{{ kebabCase name }}/{{ pascalCase name }}.stories.tsx',
+        path: 'src/{{ type }}/{{ kebabCase name }}/{{ pascalCase name }}.stories.tsx',
         templateFile: 'templates/storybook.hbs',
       },
       {
         type: 'add',
-        path: 'src/elements/{{ kebabCase name }}/{{ pascalCase name }}.scss',
+        path: 'src/{{ type }}/{{ kebabCase name }}/{{ pascalCase name }}.scss',
         templateFile: 'templates/stylesheet.hbs',
       },
       {
         type: 'add',
-        path: 'src/elements/{{ kebabCase name }}/index.ts',
+        path: 'src/{{ type }}/{{ kebabCase name }}/index.ts',
         templateFile: 'templates/index.hbs',
       },
       {
         type: 'append',
-        path: 'src/elements/index.ts',
+        path: 'src/{{ type }}/index.ts',
         template: 'export * from \'./{{kebabCase name}}\';',
       },
-    ],
-  });
-  plop.setGenerator('ds-component', {
-    description: 'add a new design system component',
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of the design system component?',
-        validate: (input: string) => {
-          if (input.includes('.')) {
-            return 'file name cannot include an extension';
-          }
-          if (input.includes(' ')) {
-            return 'file name cannot include spaces';
-          }
-          if (!input) {
-            return 'file name is required';
-          }
-          return true;
-        },
-      },
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: 'src/components/{{ kebabCase name }}/{{ kebabCase name }}.tsx',
-        templateFile: 'templates/ds.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/components/{{ kebabCase name }}/{{ kebabCase name }}.stories.tsx',
-        templateFile: 'templates/storybook.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.scss',
-        templateFile: 'templates/stylesheet.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/components/{{ kebabCase name }}/index.ts',
-        templateFile: 'templates/index.hbs',
-      },
-      {
-        type: 'append',
-        path: 'src/components/index.ts',
-        template: 'export * from \'./{{kebabCase name}}\';',
-      },
-    ],
+    ]
   });
 }
