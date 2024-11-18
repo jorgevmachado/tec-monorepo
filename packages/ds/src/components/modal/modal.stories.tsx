3759ced37e7103b5a5def208131b13cf9e334d3a
Jorge Machado <jorge.vmachado@hotmail.com>
2024-11-17 21:22:41 -0300

    implement component Modal with storybook

--- packages/ds/src/components/modal/modal.stories.tsx
+++ packages/ds/src/components/modal/modal.stories.tsx
@@ -1,19 +1,61 @@
+import * as React from 'react';
+
+import { useState } from 'react';
+
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Modal from './modal';
+import Modal, { ModalProps } from './modal';
+
+import Button from '../button';
 
 const meta = {
     args: {
-        children: 'Hello, World!',
+        title: 'title modal',
+        isOpen: false,
+        spacing: 'md',
+        context: 'primary',
+        onClose: undefined,
+        children: 'body modal',
+        closeOnEsc: true,
+        closeOnOutsideClick: true,
+        removeBackgroundScroll: false,
     },
     title: 'Components/Modal',
     component: Modal,
+    decorators: [(Story) => <div style={{ height: '50vh' }}><Story/></div>]
 } satisfies Meta<typeof Modal>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const TemplateModal = (args: ModalProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+            <Button onClick={() => setIsOpen(true)}>Abrir modal</Button>
+            <Modal {...args} isOpen={isOpen} onClose={() => setIsOpen(false)} children={args.children}/>
+        </>
+    );
+};
+
 export const Default: Story = {
-    args: { children: 'Exemplo' }
+    args: { },
+    render: (args) => <TemplateModal {...args} children={args.children}/>
+};
+
+export const WithScroll: Story = {
+    args: {
+        children: (
+            <>
+                { Array
+                    .from({ length: 32 }, (_, index) => index + 1)
+                    .map((item) => (<div key={item}>CARD {item}</div>))
+                }
+            </>
+        ),
+        removeBackgroundScroll: true,
+    },
+    render: (args) => <TemplateModal {...args} children={args.children}/>
 };
--- packages/ds/src/components/modal/modal.tsx
+++ packages/ds/src/components/modal/modal.tsx
@@ -1,16 +1,85 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { TContext, joinClass } from '../../utils';
 
 import './Modal.scss';
 
-interface ModalProps {
-children: React.ReactNode;
+import Icon from '../../elements/icon';
+import Text from '../../elements/text';
+
+export interface ModalProps {
+    title?: string;
+    isOpen: boolean;
+    spacing?: 'md' | 'lg';
+    context?: TContext;
+    onClose(): void;
+    children: React.ReactNode;
+    closeOnEsc?: boolean;
+    closeOnOutsideClick?: boolean;
+    removeBackgroundScroll?: boolean;
 }
 
-export default function Modal({ children }: ModalProps) {
-return (
-<div className="modal">
-    <h1>Modal Component</h1>
-    {children}
-</div>
-);
+export default function Modal({
+    title,
+    isOpen,
+    spacing = 'md',
+    context = 'primary',
+    onClose,
+    children,
+    closeOnEsc,
+    closeOnOutsideClick,
+    removeBackgroundScroll,
+    ...props
+}: ModalProps) {
+    
+    const onCloseFunction = () => {
+        if ( removeBackgroundScroll) {
+            document.body.style.overflow = 'auto';
+        }
+        onClose();
+    };
+
+    useEffect(() => {
+        if (removeBackgroundScroll && isOpen) {
+            document.body.style.overflow = 'hidden';
+        }
+    }, [isOpen]);
+
+    useEffect(() => {
+        if (closeOnEsc) {
+            const keyDownHandler = (event: KeyboardEvent) => {
+                if (event.key === 'Escape') {
+                    event.preventDefault();
+                    onCloseFunction();
+                }
+            };
+
+            window.addEventListener('keydown', keyDownHandler);
+
+            return () => window.removeEventListener('keydown', keyDownHandler);
+        }
+    }, [closeOnEsc]);
+    
+    
+    return  isOpen ? (
+        <>
+            <div
+                className="modal__backdrop modal__fade-in"
+                onClick={() => closeOnOutsideClick && onCloseFunction}/>
+            <div {...props} className={joinClass(['modal', 'modal__fade-in', `modal__spacing--${spacing}`])}>
+                <Icon
+                    icon="close"
+                    onClick={onCloseFunction}
+                    tabIndex={0}
+                    className="modal__close"
+                    aria-label="close modal"
+                    data-testid="on-close"
+                />
+
+                <Text tag="h2" weight="bold" variant="xlarge" tabIndex={0}>{title}</Text>
+
+                <div tabIndex={0}>{children}</div>
+            </div>
+        </>
+    ) : null;
 };
